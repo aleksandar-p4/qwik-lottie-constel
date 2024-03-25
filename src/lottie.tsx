5abe179891c0e25c27e54c8eb61f7656f5986057
aleksandar-p4 <aleksandar.pajic@constel.co>
2024-03-25 10:03:45 +0100

    Update lottie file

--- src/lottie.tsx
+++ src/lottie.tsx
@@ -3,8 +3,7 @@ import {
   useStore,
   useSignal,
   noSerialize,
-  useOnWindow,
-  $,
+  useVisibleTask$,
 } from "@builder.io/qwik";
 import lottie from "lottie-web";
 import { Options } from "./types";
@@ -19,23 +18,21 @@ export const QwikLottie = component$(({ options }: OptionsProps) => {
   });
   const canvas = useSignal<Element>();
 
-  useOnWindow(
-    "load",
-    $(() => {
-      store.anim = noSerialize(
-        lottie.loadAnimation({
-          container: options.container || canvas.value,
-          renderer: options.renderer || "svg",
-          loop: options.loop || true,
-          autoplay: options.autoplay || true,
-          animationData: options.animationData,
-          path: options.path,
-          rendererSettings: options.rendererSettings,
-          name: options.name,
-        })
-      );
-    })
-  );
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(() => {
+    store.anim = noSerialize(
+      lottie.loadAnimation({
+        container: options.container || canvas.value,
+        renderer: options.renderer || "svg",
+        loop: options.loop || true,
+        autoplay: options.autoplay || true,
+        animationData: options.animationData,
+        path: options.path,
+        rendererSettings: options.rendererSettings,
+        name: options.name,
+      })
+    );
+  });
 
   return <div ref={canvas}></div>;
 });
